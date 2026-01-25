1ba9c484ad8948f4616a69f77853dcf66bc20f1f
Leakey <otienoleakey287@gmail.com>
2026-01-25 08:39:57 +0300

    Add custom user model with date_of_birth and profile_photo fields

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add new fields to the user list view
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    
+    # fieldsets controls the "Edit User" page
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+    # add_fieldsets controls the "Add User" page
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Customizing the Admin interface
 class BookAdmin(admin.ModelAdmin):
