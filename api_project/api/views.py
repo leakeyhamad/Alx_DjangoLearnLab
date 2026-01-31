3cd215206f17fedcb6ab70ded0c26911514c175f
Leakey <otienoleakey287@gmail.com>
2026-01-31 10:15:19 +0300

    Added CRUD operations to api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
 
+# Existing ListAPIView
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for all CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
